da30c5b5fd983d48af0efaec58fb6051b2e7f066
Valera <valerakaktotak@gmail.com>
2022-11-09 20:38:55 +0200

    add provider in app.js from react-redux

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import {
 } from "react-router-dom";
 import HomePage from "./components/Home-page/HomePage";
 import Header from "./components/Header/Header";
+import {Provider} from "react-redux";
+import store from "./redux/store";
 
 function App() {
-  return (
-      <BrowserRouter>
-          <div className="App">
-              <Header />
-              <Routes>
-                  <Route path="/" element={<HomePage />} />
-              </Routes>
-          </div>
-      </BrowserRouter>
-  );
+    return (
+        <Provider store={store}>
+            <BrowserRouter>
+                <div className="App">
+                    <Header/>
+                    <Routes>
+                        <Route path="/" element={<HomePage/>}/>
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </Provider>
+    );
 }
 
 export default App;
