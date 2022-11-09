4e96b478d451fa13e8bca95422cbd4761afadc89
Valera <valerakaktotak@gmail.com>
2022-11-09 21:25:37 +0200

    add function with add game to cart state

--- src/components/Game-buy/GameBuy.jsx
+++ src/components/Game-buy/GameBuy.jsx
@@ -1,11 +1,19 @@
 import s from './GameBuy.module.css'
+import {useDispatch} from "react-redux";
 import Button from "../Button/Button";
+import {setItemInCart} from "../../redux/Cart-reducer/Cart-reducer";
+
 
 const GameBuy = ({game}) => {
+    const dispatch = useDispatch();
+    const cartHandle = (e) => {
+        e.stopPropagation();
+        dispatch(setItemInCart(game))
+    }
     return(
         <div className={s.game_buy}>
             <span>{game.price} руб.</span>
-            <Button onClick={()=>null} type="primary" >
+            <Button onClick={ cartHandle } type="primary" >
                 В корзину
             </Button>
         </div>
