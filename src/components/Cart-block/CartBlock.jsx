0d2000a84bd8dcf17ba0d7226905a0408b07e6a2
Valera <valerakaktotak@gmail.com>
2022-11-09 23:24:59 +0200

    add total price in car

--- src/components/Cart-block/CartBlock.jsx
+++ src/components/Cart-block/CartBlock.jsx
@@ -1,11 +1,14 @@
 import s from './CartBlock.module.css'
 import { BiCartAlt } from 'react-icons/bi';
+import {useSelector} from "react-redux";
 
 const CartBlock = () => {
+    const games = useSelector(state => state.cart.itemsInCart);
+    const totalCartCost = games.reduce((acc, game) => acc += game.price, 0)
     return(
         <div className={s.cartBlock}>
             <BiCartAlt size={25} className={s.cartBlock__cart} />
-            <span className={s.cartBlock__prise}>2578 руб.</span>
+            <span className={s.cartBlock__prise}>{totalCartCost} руб.</span>
         </div>
     )
 }
