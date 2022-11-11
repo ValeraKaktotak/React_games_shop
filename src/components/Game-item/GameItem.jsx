3c4c370aea22c378befaea1a98d162c017639985
Valera <valerakaktotak@gmail.com>
2022-11-11 15:31:17 +0200

    add game page and did some small fixes

--- src/components/Game-item/GameItem.jsx
+++ src/components/Game-item/GameItem.jsx
@@ -4,21 +4,22 @@ import GameBuy from "../Game-buy/GameBuy";
 import {useDispatch} from "react-redux";
 import {useNavigate} from "react-router-dom";
 import {setCurrentGame} from "../../redux/Game-reducer/Game-reducer";
+import GameGenre from "../Game-genre/GameGenre";
 
 const GameItem = ({game}) => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const handleClick = () => {
         dispatch(setCurrentGame(game))
-        navigate(`/app/${game.title}`)
+        navigate(`/${game.title}`)
     }
     return(
         <div className={s.game_item} onClick={handleClick}>
             <GameCover image={game.image}/>
             <span className={s.title}>{game.title}</span>
-            <div className={s.ganres_block}>
-                {game.genres.map((ganre, index) => {
-                    return <span key={index} className={s.ganre_item}>{ganre}</span>
+            <div className={s.genres_block}>
+                {game.genres.map((genre, index) => {
+                    return <GameGenre key={genre} genre={genre} />
                 })}
             </div>
             <div className={s.buy}>
